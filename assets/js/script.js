1cbb6caaf6485242dafb4cdf38eb083c6143a55e
Luyan Zhang <z709973733@gmail.com>
2022-03-26 22:49:37 -0400

    final version with comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+//saved tasks in localstorage
 var tasks = {
   "9":"",
   "10":"",
@@ -10,6 +11,7 @@ var tasks = {
   "17":""
 };
 
+//unsaved tasks
 var temptasks = {
   "9":"",
   "10":"",
@@ -35,6 +37,7 @@ var loadTasks = function() {
     var current_task = "#" + key;
     auditTask($(current_task));
   });
+  document.getElementById("currentDay").textContent = "Today is: " + moment().format('YYYY-MM-DD');
 };
 
 var saveTasks = function() {
@@ -42,31 +45,31 @@ var saveTasks = function() {
 };
 
 var auditTask = function(current_task) {
-
-  // get date from task element
+  // get current hour
   var current_time = moment().format("HH:mm");
   var current_hour = current_time.slice(0, 2);
+  // get hour from task element
   var selected_task = current_task.parent(".row").children("h3");
   var selected_time = selected_task.text().slice(0, 2);
   current_task.removeClass("list-group-item-warning list-group-item-danger list-group-item-success");
-
+  //incoming tasks
   if (current_hour < selected_time) {
     $(current_task).addClass("list-group-item-warning");
-  } 
+  } //current tasks
   else if (current_hour == selected_time) {
     $(current_task).addClass("list-group-item-danger");
-  } else if (current_hour > selected_time) {
+  } //finished tasks
+  else if (current_hour > selected_time) {
     $(current_task).addClass("list-group-item-success");
   }
 };
-
-
+// save text to temptasks when finish edit
 $(".container").on("blur", "textarea", function() {
   var text = $(this).val();
   var current_id = this.id;
   temptasks[current_id] = text;
 });
-
+// add temptasks to savedtasks when click on button
 $(".container").on("click", "button", function() {
   var current = ($(this).parent(".row").children("textarea"));
   var current_id = current[0].id;
